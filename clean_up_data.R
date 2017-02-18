c53deb291a2bbd9a5491a32fb68b22e83812d09b
Domingo <domingohui@gmail.com>
2017-02-17 22:27:49 -0500

    use variable

--- clean_up_data.R
+++ clean_up_data.R
@@ -1,7 +1,7 @@
 # dataset=read.csv("dataset/diabetes-training.csv")
 
 encoding <- function (dataset, col_name, from, to) {
-  new_col <- as.numeric(factor(dataset$race),
+  new_col <- as.numeric(factor(dataset[[col_name]]),
                         levels = from,
                         labels = to)
   return (new_col)

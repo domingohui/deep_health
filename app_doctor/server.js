5553a39e14af614ba37b5c5b1175d252612768d8
Domingo <domingohui@gmail.com>
2017-02-19 04:23:21 -0500

    running backend for GETting attr's

--- app_doctor/server.js
+++ app_doctor/server.js
@@ -3,12 +3,18 @@ const app = express();
 const port = 5000;
 const path = require('path');
 
+const Attributes = require( './Attributes');
+
 // Static files
 app.use('/static', express.static(path.join(__dirname + '/static')));
 
 // Routes
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname + '/static/index.html'));
+});
+
+app.get('/get_attributes/', (req, res) => {
+    res.json(Attributes.get_attributes_and_options());
 });
 
 app.listen(port, (err) => {
